ef2d255c02b719ac203cb5f1fdd33b5a79f7bb39
Dokey199X <122604130+Dokey199X@users.noreply.github.com>
2023-01-25 09:58:31 +0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,76 +1,65 @@
 import random
 import string
-import sys
 from tkinter import *
-
 import cv2
 import mediapipe as mp
 import numpy as np
-import pandas as pd
 from PIL import Image, ImageTk
-
 
 def getRan(count):
     setLet = random.sample(string.ascii_uppercase, count)
     return setLet
 
+# Shows the detected letter from sign language
+def show_letter():
+    detected_letter_frame = Frame(scrlevone, width=900, height=200,bg='red').place(x=0, y=600)
+    detected_label = Label(
+        detected_letter_frame, text=str(y_pred[0]), font=("Courier", 50, "bold"), fg="black",bg='white'
+    )
+    detected_label.grid(row=3,column=0)
+    detected_label.after(20, show_letter)
+
+# Displays the random letters
+def display_letter():
+    letter = Frame(scrlevone, width=1920, height=300, bg='blue').place(x=0, y=30)
+
+    for i in range(len(rone)):
+        letDisp = Label(
+            letter, text=rone[i], font=("Courier", 150, "bold"), fg="black"
+        )
+        letDisp.grid(row=2, column=i)
 
 def image_processed(hand_img):
-
-    # Image processing
-    # 1. Convert BGR to RGB
-    img_rgb = cv2.cvtColor(hand_img, cv2.COLOR_BGR2RGB)
-
-    # 2. Flip the img in Y-axis
-    img_flip = cv2.flip(img_rgb, 1)
-
-    # accessing MediaPipe solutions
-    mp_hands = mp.solutions.hands
-
-    # Initialize Hands
-    hands = mp_hands.Hands(
-        static_image_mode=True, max_num_hands=1, min_detection_confidence=0.7
-    )
-
-    # Results
-    output = hands.process(img_flip)
-
+    img_rgb = cv2.cvtColor(hand_img, cv2.COLOR_BGR2RGB) # 1. Convert BGR to RGB
+    img_flip = cv2.flip(img_rgb, 1) # 2. Flip the img in Y-axis
+    mp_hands = mp.solutions.hands # Accessing MediaPipe solutions
+    hands = mp_hands.Hands(static_image_mode=True, max_num_hands=1, min_detection_confidence=0.7) # Initialize Hands
+    output = hands.process(img_flip) # Results
     hands.close()
 
     try:
         data = output.multi_hand_landmarks[0]
-        # print(data)
         data = str(data)
-
         data = data.strip().split("\n")
-
         garbage = ["landmark {", "  visibility: 0.0", "  presence: 0.0", "}"]
-
         without_garbage = []
-
         for i in data:
             if i not in garbage:
                 without_garbage.append(i)
-
         clean = []
-
         for i in without_garbage:
             i = i.strip()
             clean.append(i[2:])
-
         for i in range(0, len(clean)):
             clean[i] = float(clean[i])
         return clean
     except:
         return np.zeros([1, 63], dtype=int)[0]
 
-
 import pickle
 
-# load model
 with open("model.pkl", "rb") as f:
     svm = pickle.load(f)
-
 
 rone = getRan(3)
 
@@ -90,62 +79,23 @@ width, height = 320, 240
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-
-def show_frames():
-    # Get the latest frame and convert into Image
-    ret, frame = cap.read()
-
-    # frame = cv.flip(frame,1)
+def camdisplay():
+    global y_pred
+    ign, frame = cap.read()
     data = image_processed(frame)
-    # print(data.shape)
     data = np.array(data)
     y_pred = svm.predict(data.reshape(-1, 63))
     print(y_pred)
 
-    # font
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    # org
-    org = (50, 100)
-    # fontScale
-    fontScale = 3
-    # Blue color in BGR
-    color = (255, 0, 0)
-    # Line thickness of 2 px
-    thickness = 5
-
-    cv2.putText(
-        frame,
-        str(y_pred[0]),
-        org,
-        font,
-        fontScale,
-        color,
-        thickness,
-        cv2.LINE_AA,
-    )
-
-    cv2image = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2RGB)
-    img = Image.fromarray(cv2image)
-    # Convert image to PhotoImage
-    imgtk = ImageTk.PhotoImage(image=img)
+    cv2.putText(frame, str(y_pred[0]), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 2, cv2.LINE_AA)
+    frameRGBA = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    img = Image.fromarray(frameRGBA)
+    imgtk = ImageTk.PhotoImage(image = img)
     vidCap.imgtk = imgtk
     vidCap.configure(image=imgtk)
+    vidCap.after(10, camdisplay)
 
-    # Repeat after an interval to capture continiously
-    vidCap.after(20, show_frames)
-
-
-show_frames()
-
-letter = Frame(scrlevone, width=1920, height=680).place(x=0, y=0)
-
-
-for i in range(len(rone)):
-    label2 = Label(
-        letter, text=rone[i], font=("Courier", 150, "bold"), fg="black"
-    )
-    # label2.config(anchor=CENTER)
-    label2.pack(side=LEFT, fill=X, ipadx=10, ipady=10, expand=TRUE)
-
-
+camdisplay()
+show_letter()
+display_letter()
 scrlevone.mainloop()
